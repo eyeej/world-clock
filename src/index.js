969c70bfd70d6ae968e75cddd79337aae946e141
eyeej <eyee.joy@gmail.com>
2025-08-13 16:31:35 +1000

    js and css changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function updateTime() {
-    let losAngelesElement = document.querySelector("#los-angeles");
+    let losAngelesElement = document.getElementById("los-angeles");
 
-    if(losAngelesDateElement) {
+    if(losAngelesElement) {
         let losAngelesDateElement = losAngelesElement.querySelector(".date");
         let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
@@ -10,6 +10,19 @@ function updateTime() {
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format("HH:mm:ss [<small>]A[</small>]");
     }
+
+    let sydneyElement = document.getElementById("sydney");
+
+    if(sydneyElement) {
+        let sydneyDateElement = sydneyElement.querySelector(".date");
+        let sydneyTimeElement = sydneyElement.querySelector(".time");
+
+        let sydneyTime = moment().tz("Australia/Sydney");
+
+sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+sydneyTimeElement.innerHTML = sydneyTime.format("HH:mm:ss [<small>]A[</small>]");
+    }
 }
+
 updateTime();
 setInterval(updateTime, 1000);
