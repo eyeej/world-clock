c70a5ab4e7439285de19254bb5e3ea74d74a9241
eyeej <eyee.joy@gmail.com>
2025-08-13 16:12:55 +1000

    js changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-function updateTime()
-
-let losAngelesElement = document.querySelector("#los-angeles");
-if(losAngelesDateElement) {
-    losAngelesDateElement = losAngelesElement.querySelector("#.date");
-    losAngelesTimeElement = losAngelesElement.querySelector("#.time");
-    let losAngelesTime = moment().tz("America/Los_Angeles");
+function updateTime() {
+    let losAngelesElement = document.querySelector("#los-angeles");
+    if(losAngelesDateElement) {
+        let losAngelesDateElement = losAngelesElement.querySelector("#.date");
+        let losAngelesTimeElement = losAngelesElement.querySelector("#.time");
+        let losAngelesTime = moment().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format("HH:mm:ss" [<small>]A[</small>]");
+losAngelesTimeElement.innerHTML = losAngelesTime.format("HH:mm:ss [<small>]A[</small>]");
 
 }
-
+updateTime();
+setInterval(updateTime, 1000);
