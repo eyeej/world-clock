5f9d64e2f361ddb77fc75ff9ff9f97ec9e60d18f
eyeej <eyee.joy@gmail.com>
2025-08-14 13:03:58 +1000

    js adjustments made with chatgpt

--- src/index.js
+++ src/index.js
@@ -26,19 +26,20 @@ sydneyTimeElement.innerHTML = sydneyTime.format("HH:mm:ss [<small>]A[</small>]")
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
-    let citiesSelectElement = document.querySelector("#cities");
-    citiesSelectElement.innerHTML = cityTimeZone;
-    citiesElement = `
+
+    let citiesContainer = document.querySelector("#cities");
+    citiesContainer.innerHTML = `
     <div class="city">
         <div>
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-    <div class="time">>${cityTime.format("HH:mm:ss")} <small>${cityTime.format("A")}</small></div>
+        <div class="time">${cityTime.format("HH:mm:ss")} <small>${cityTime.format("A")}</small></div>
     </div>
-    `;
+  `;
 
 }
 
@@ -46,5 +47,4 @@ updateTime();
 setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
-
-citiesSelectElement = addEventListener("change", updateCity);
+citiesSelectElement.addEventListener("change", updateCity);
